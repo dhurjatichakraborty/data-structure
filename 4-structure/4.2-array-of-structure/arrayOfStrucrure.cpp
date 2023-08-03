5fe140c86b14230a378f15c4a2e612a7aad687e6
Dhurjati Chakraborty <mail.dhurjati.chakraborty@gmail.com>
2023-08-04 00:41:54 +0530

    arrayOfStrucrure.cpp

--- 4-structure/4.2-array-of-structure/arrayOfStrucrure.cpp
+++ 4-structure/4.2-array-of-structure/arrayOfStrucrure.cpp
@@ -1,6 +1,25 @@
 using namespace std;
 #include <iostream>
+#define SIZE 10
+
+struct structure_array
+{
+    int data;
+} var[SIZE];
 
 int main()
 {
+    int i;
+    for (i = 0; i < SIZE; i++)
+    {
+        cout << "var[" << i << "].data = ";
+        cin >> var[i].data;
+    }
+
+    cout << "\nThe elements inside the array of structure variable VAR\n";
+    cout << "--------------------------------------------------------\n";
+    for (i = 0; i < SIZE; i++)
+        cout << var[i].data << " ";
+    cout << endl;
+    return 0;
 }
