d411dca9de5726c9e26d758bd5c2042a9ed2e12c
dhurjatichakraborty <mail.dhurjati.chakraborty@gmail.com>
2023-08-04 13:17:02 +0530

    second largest

--- 2-arrays/array_second_largest.cpp
+++ 2-arrays/array_second_largest.cpp
@@ -2,32 +2,39 @@
  * This program finds out the largest element amongst an integer array. It also prints all elements present
  */
 
-// using namespace std;
-// #include <iostream>
+#include <iostream>
+#include <limits>
+using namespace std;
 
-// int main()
-// {
-//     int numbers[] = {199, 8, 7, 17, 99};
-//     int i, largest = 0, second_largest = 0, size;
-//     size = sizeof(numbers) / sizeof(numbers[0]);
-//     for (i = 1; i < size; i++)
-//     {
-//         if (numbers[i] > largest)
-//         {
-//             second_largest = largest;
-//             largest = numbers[i];
-//         }
-//         else if (numbers[i] > second_largest && numbers[i] != largest)
-//         {
-//             second_largest = numbers[i];
-//         }
-//     }
-//     cout << "Elements in the array are:" << endl;
-//     for (i = 0; i < size; i++)
-//         cout << numbers[i] << " ";
+int main()
+{
+    int numbers[] = {199, 8, 7, 100, 99};
+    int i, largest, second_largest, size;
 
-//     cout << "\nThe largest number is: " << largest << endl;
-//     cout << "The second largest number is: " << second_largest << endl;
+    size = sizeof(numbers) / sizeof(numbers[0]);
+    largest = second_largest = INT32_MIN;
 
-//     return 0;
-// }
+    for (i = 0; i < size; i++)
+    {
+        if (numbers[i] > largest)
+        {
+            second_largest = largest;
+            largest = numbers[i];
+        }
+        else if (numbers[i] > second_largest && numbers[i] < largest)
+        {
+            second_largest = numbers[i];
+        }
+    }
+
+    cout << "The array elements are:\n";
+    for (i = 0; i < size; ++i)
+    {
+        cout << numbers[i] << " ";
+    }
+    cout << "\nThe largest element in the given array is: " << largest;
+    cout
+        << "\nThe second largest element in the given array is: " << second_largest << endl;
+
+    return 0;
+}
